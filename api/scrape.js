61fec815621b6e838db8d2429f552e5373566551
nasserzir <54932634+nasserzir@users.noreply.github.com>
2025-06-08 11:47:06 +0200

    Update scrape.js

--- api/scrape.js
+++ api/scrape.js
@@ -1,3 +1,24 @@
-export default function handler(req, res) {
-  res.status(200).json({ hello: "world" });
-}
+// api/scrape.js
+
+const axios = require("axios");
+
+module.exports = async (req, res) => {
+  const { url } = req.query;
+
+  if (!url) {
+    return res.status(400).json({ error: "Missing URL parameter" });
+  }
+
+  try {
+    const response = await axios.get("https://app.scrapingbee.com/api/v1", {
+      params: {
+        api_key: process.env.SCRAPINGBEE_API_KEY,
+        url,
+      },
+    });
+
+    res.status(200).send(response.data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
